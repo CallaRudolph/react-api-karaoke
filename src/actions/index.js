6310016d69a24b7b27379530454969f51a4d8c3a
Calla Rudolph <callarudolph@gmail.com>
2017-09-24 20:24:39 -0700

    install thunk to retrieve first api call

--- src/actions/index.js
+++ src/actions/index.js
@@ -16,22 +16,22 @@ export const selectSong = (songId) => ({
   songId
 });
 
-export function fetchSongId(title) {
-  return function (dispatch) {
-    const localSongId = v4();
-    dispatch(requestSong(title, localSongId));
-    title = title.replace(" ", "_");
-    return fetch("http://api.musixmatch.com/ws/1.1/track.search?&q_track=" + title + "&page_size=1&s_track_rating=desc&apikey=YOUR-API-KEY-HERE").then(
-      response => response.json(),
-      error => console.log("An error occured.", error)
-    ).then(function(json) {
-      console.log("CHECK OUT THIS SWEET API RESPONSE:", json)
-    });
-  };
-}
-
 export const requestSong = (title, localSongId) => ({
   type: types.REQUEST_SONG,
   title,
   songId: localSongId
 });
+
+export function fetchSongId(title) {
+  return function (dispatch) {
+    const localSongId = v4();
+    dispatch(requestSong(title, localSongId));
+    title = title.replace(" ", "_");
+    return fetch("http://api.musixmatch.com/ws/1.1/track.search?&q_track=" + title + "&page_size=1&s_track_rating=desc&apikey=bc6b2256711a5fcbd1081e4f496f7b12").then(
+      response => response.json(),
+      error => console.log("An error occurred.", error)
+    ).then(function(json) {
+      console.log("CHECK OUT THIS SWEET API RESPONSE:", json)
+    });
+  };
+}
